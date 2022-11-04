0ba93217a8cb685424f2e72520dbd31e81aa0113
심야 <ximya@sim-yaui-MacBookPro.local>
2022-11-04 15:39:06 +0900

    feature : BottomNavigation Bar 추가

--- lib/ui/screens/tabs/tabs_view_model.dart
+++ lib/ui/screens/tabs/tabs_view_model.dart
@@ -1,3 +1,19 @@
-import 'package:uppercut_fantube/ui/base/base_view_model.dart';
+import 'package:uppercut_fantube/utilities/index.dart';
 
-class TabsViewModel extends BaseViewModel {}
+class TabsViewModel extends BaseViewModel {
+  /* Variables */
+  /// State Variables
+  // 네비게이션 바 인덱스
+  RxInt selectedTabIndex = 0.obs;
+
+
+
+  /* Intents */
+  /// UI Interaction
+  // 네비게이션 바 아이템이 선택 되었을 때
+  void onBottomNavBarItemTapped(int index) {
+    selectedTabIndex(index);
+  }
+
+
+}
