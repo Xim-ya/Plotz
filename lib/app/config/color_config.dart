ea33cb2e1c1053080f07d34af20cfa812ab97d41
심야 <ximya@sim-yaui-MacBookPro.local>
2022-11-05 15:31:14 +0900

    refactor : 홈 스크린 리팩토링(scaffold 작업) + Maetrial Icno Btn 모듈 추가

--- lib/app/config/color_config.dart
+++ lib/app/config/color_config.dart
@@ -6,4 +6,5 @@ abstract class AppColor {
 
   static const black = Color(0xFF0F0F0F);
   static const darkGrey = Color(0xFF212121); // Used in Bottom Navigation
+  static const lightGrey = Color(0xFFBBBAC1); // Used in Home Sub title;
 }
--- lib/presentation/screens/home/home_scaffold.dart
+++ lib/presentation/screens/home/home_scaffold.dart
@@ -1,12 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+import 'package:uppercut_fantube/app/config/app_space_config.dart';
+import 'package:uppercut_fantube/app/config/font_config.dart';
+import 'package:uppercut_fantube/presentation/common/icon_ink_well_button.dart';
 import 'package:uppercut_fantube/utilities/index.dart';
 
 class HomeScaffold extends StatelessWidget {
-  const HomeScaffold({Key? key, required this.animationAppbar, required this.scrollController, required this.body})
+  const HomeScaffold({Key? key, required this.animationAppbar, required this.scrollController, required this.body, required this.appBarHeight, required this.stackedGradientPosterBg, required this.contentSlider})
       : super(key: key);
 
   final List<Widget> animationAppbar;
+  final List<Widget> stackedGradientPosterBg;
+  final Widget contentSlider;
   final Widget body;
   final ScrollController scrollController;
+  final double appBarHeight;
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +25,25 @@ class HomeScaffold extends StatelessWidget {
         children: [
           SingleChildScrollView(
             controller: scrollController,
-            child: body,
+            child:  Stack(
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    for (final widget in stackedGradientPosterBg) widget,
+                    /* 컨텐츠 섹션 */
+                    Column(
+                      children: [
+                        SizedBox(height: appBarHeight), // 커스텀 앱바와 간격을 맞추기 위한 위젯
+                        AppSpace.size72,
+                        contentSlider,
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-
-          for (final tab in animationAppbar) tab,
+          for (final widget in animationAppbar) widget,
         ],
       ),
     );
