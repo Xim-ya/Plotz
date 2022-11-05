27261fb51a755b725b997853f194f8cebcb5df95
심야 <ximya@sim-yaui-MacBookPro.local>
2022-11-05 17:19:24 +0900

    feature : 홈 스크린 > 카테고리별 컨텐츠 슬라이더 UI 추가

--- lib/app/config/color_config.dart
+++ lib/app/config/color_config.dart
@@ -7,4 +7,5 @@ abstract class AppColor {
   static const black = Color(0xFF0F0F0F);
   static const darkGrey = Color(0xFF212121); // Used in Bottom Navigation
   static const lightGrey = Color(0xFFBBBAC1); // Used in Home Sub title;
+  static const mixedWhite = Color(0xFFE6E6E6);
 }
--- lib/presentation/screens/home/home_scaffold.dart
+++ lib/presentation/screens/home/home_scaffold.dart
@@ -7,13 +7,14 @@ import 'package:uppercut_fantube/presentation/common/icon_ink_well_button.dart';
 import 'package:uppercut_fantube/utilities/index.dart';
 
 class HomeScaffold extends StatelessWidget {
-  const HomeScaffold({Key? key, required this.animationAppbar, required this.scrollController, required this.body, required this.appBarHeight, required this.stackedGradientPosterBg, required this.contentSlider})
+  const HomeScaffold({Key? key, required this.animationAppbar, required this.scrollController, required this.body, required this.appBarHeight, required this.stackedGradientPosterBg, required this.topExposedContentSlider, required this.topTenContentSlider})
       : super(key: key);
 
   final List<Widget> animationAppbar;
   final List<Widget> stackedGradientPosterBg;
-  final Widget contentSlider;
-  final Widget body;
+  final Widget topExposedContentSlider;
+  final List<Widget> topTenContentSlider;
+  final List<Widget> body;
   final ScrollController scrollController;
   final double appBarHeight;
 
@@ -30,14 +31,17 @@ class HomeScaffold extends StatelessWidget {
                 Stack(
                   children: <Widget>[
                     for (final widget in stackedGradientPosterBg) widget,
-                    /* 컨텐츠 섹션 */
-                    Column(
-                      children: [
-                        SizedBox(height: appBarHeight), // 커스텀 앱바와 간격을 맞추기 위한 위젯
-                        AppSpace.size72,
-                        contentSlider,
-                      ],
-                    ),
+                  ],
+                ),
+                /* 컨텐츠 섹션 */
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: appBarHeight), // 커스텀 앱바와 간격을 맞추기 위한 위젯
+                    AppSpace.size72,
+                    topExposedContentSlider, // 상단 대표 컨텐츠 슬라이더
+                    for (final widget in topTenContentSlider) widget, // Top10 컨텐츠 슬라이더
+                    for (final widget in body) widget,
                   ],
                 ),
               ],
