7d0bdee39160358098f4a82c40bcc89fc605e4cc
심야 <ximya@sim-yaui-MacBookPro.local>
2022-11-05 18:08:56 +0900

    refactor : app bar blur activate 적용 여부 로직 리팩토링. 불필요 변수 할당 최소화

--- lib/presentation/screens/home/home_view_model.dart
+++ lib/presentation/screens/home/home_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/rendering.dart';
 import 'package:uppercut_fantube/app/config/size_config.dart';
+import 'package:uppercut_fantube/utilities/extension/prevent_duplicate_value_inject.dart';
 import 'package:uppercut_fantube/utilities/index.dart';
 
 class HomeViewModel extends BaseViewModel {
@@ -10,31 +11,38 @@ class HomeViewModel extends BaseViewModel {
   final RxBool showAppbarBackground = true.obs;
   RxBool showBlurAtAppBar = false.obs;
 
-
   /// Size
   final double appBarHeight = SizeConfig.to.statusBarHeight + 56;
 
   ///  Controllers
   late ScrollController scrollController;
 
-
-  // Intent
+  /* Intent */
   /// UI Intent Method
+  // AppBar Blur 처리 여부 로직
   void turnOnBlurInAppBar() {
-    if (scrollOffset == 0) {
+    // Status Bar Height 보다 offest이 작을 땐 Blur 처리 X
+    if (scrollOffset <= SizeConfig.to.statusBarHeight) {
       showBlurAtAppBar(false);
       return;
     } else {
+      /** 중복 할당을 방지하기 위해. 조건 두가지를 추가.
+       * [scrollController.position.userScrollDirection] 유저가 아래로 스크롤하고
+       * [showBlurAtAppBar.isTrue] blur값이 true로 선언되어 있다면 값을 변경.
+       * */
       if (scrollController.position.userScrollDirection ==
-          ScrollDirection.forward) {
-        showBlurAtAppBar(false);
+          ScrollDirection.forward && showBlurAtAppBar.isTrue) {
+          showBlurAtAppBar(false);
         return;
-      } else {
+      } else if  (scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse && showBlurAtAppBar.isFalse){
         showBlurAtAppBar(true);
-
+        return;
       }
     }
   }
+
+
 
   @override
   void onInit() {
