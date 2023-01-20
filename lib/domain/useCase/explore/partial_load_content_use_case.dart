eed11cfe197295c6472fa24b25860c57f4ae91ef
Xim-ya <hj_lee_com@naver.com>
2023-01-20 14:20:07 +0900

    refactor : 탐색 컨텐츠 > 부분 호출 및 업데이트 로직 리팩토링

---
+++ lib/domain/useCase/explore/partial_load_content_use_case.dart
@@ -0,0 +1,10 @@
+import 'package:uppercut_fantube/utilities/index.dart';
+
+class ParticalLoadContentUseCase extends BaseUseCase<int, List<ExploreContent>> {
+  @override
+  Future<List<ExploreContent>> call(int swiperIndex) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+
+}
