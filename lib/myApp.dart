a6d6fa50a720827e311fab5a6c6fedbbb244f2d7
XimYa <hj_lee_com@naver.com>
2023-03-27 16:57:13 +0900

    chore : 불필요 코드 삭제

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,40 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:soon_sak/utilities/index.dart';
-import 'firebase_options.dart';
 import 'presentation/common/layout/response_layout_builder.dart';
 
-// /// Root
-// /// 초기화 메소드 순서 중요 (변경X)
-// void main() async {
-//   // Flutter Engine 초기화
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await dotenv.load(fileName: '.env');
-//
-//   // Isolate 토큰 생성 및 초기화
-//   final RootIsolateToken rootIsolateToken = RootIsolateToken.instance!;
-//   BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
-//
-//   /// FireBase 초기화
-//   // Firebase Crashlytics 설정
-//   await runZonedGuarded<Future<void>>(() async {
-//     await Firebase.initializeApp(
-//       name: dotenv.env['FIREBASE_KEY'],
-//       options: ProdFirebaseOptions.currentPlatform,
-//     );
-//
-//     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-//     Get.put(AppAnalytics(), permanent: true);
-//     await AppAnalytics.instance.setAnalyticsCollectionEnabled(true);
-//     await AppAnalytics.instance.logAppOpen();
-//
-//
-//     // Portrait 고정
-//     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-//         .then((_) {
-//       runApp(const MyApp());
-//     });
-//   }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
