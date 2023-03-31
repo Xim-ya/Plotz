fd3241121a0c2490b8c0ed4509ffa6f3a2bd895a
XimYa <hj_lee_com@naver.com>
2023-03-31 11:10:58 +0900

    chore : 불필요 코드 삭제

--- lib/presentation/screens/contentDetail/content_detail_binding.dart
+++ lib/presentation/screens/contentDetail/content_detail_binding.dart
@@ -22,12 +22,6 @@ class ContentDetailBinding extends Bindings {
 
     // fenix를 true로 설정하는것도 고려해볼 수 있음
     Get.lazyPut(() => LoadContentDetailInfoUseCase(Get.find()), fenix: false);
-    // Get.lazyPut(() => LoadContentCreditInfoUseCase(Get.find()), fenix: false);
-    // Get.lazyPut(() => LoadContentImgListUseCase(Get.find()), fenix: false);
-    // // Video
-    // Get.lazyPut(() => LoadContentOfVideoListUseCase(Get.find()), fenix: true);
-
-    // Get.put(LoadContentDetailInfoUseCase(Get.find()));
     Get.put(LoadContentCreditInfoUseCase(Get.find()));
     Get.put(LoadContentImgListUseCase(Get.find()));
     Get.put(LoadContentOfVideoListUseCase(Get.find()));
