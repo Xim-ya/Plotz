f5351056d4c8fd8423ce60162ebe7b623ecb2329
XimYa <hj_lee_com@naver.com>
2023-05-08 23:47:39 +0900

    feature : homeScreen 레이아웃 변경
    - top Banner 구조 변경.
    - top Banner fade out 애니메이션 추가
    - 상단 노출 카테골 컬렉션 슬라이더 추가

--- lib/presentation/screens/home/new_home_screen.dart
+++ lib/presentation/screens/home/new_home_screen.dart
@@ -1,4 +1,5 @@
-import 'dart:ui';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:soon_sak/presentation/common/image/new_content_post_item.dart';
 import 'package:soon_sak/presentation/screens/home/localWidget/new_home_scaffold.dart';
 import 'package:soon_sak/presentation/screens/home/localWidget/paged_category_list_view.dart';
 import 'package:soon_sak/utilities/index.dart';
@@ -12,41 +13,102 @@ class NewHomeScreen extends BaseScreen<HomeViewModel> {
   @override
   Widget buildScreen(BuildContext context) {
     return NewHomeScaffold(
-      animationAppbar: _buildAnimationAppbar(),
+      stackedTopGradient: _buildStackedTopGradient(),
+      stackedAppBar: _buildStackedAppBar(),
+      topBannerSlider: const _BannerSlider(),
+      topTenContentSlider: const _TopTenContentSlider(),
       scrollController: vm.scrollController,
       stackedGradientPosterBg: _buildStackedGradientPosterBg(),
-      topBannerSlider: _buildTopBannerSlider(),
-      topTenContentSlider: _buildTopTenContentSlider(),
       categoryContentCollectionList: _buildCategoryCollection(),
+      topPositionedContentSliderList: _buildTopPositionedContentSliderList(),
     );
   }
 
-  /// 상단 배너 슬라이더
-  Widget _buildTopBannerSlider() => Obx(
-        () => CarouselSlider.builder(
-          carouselController: vm.carouselController,
-          itemCount: vm.bannerContentList?.length ?? 2,
-          options: CarouselOptions(
-            autoPlay: true,
-            onPageChanged: (index, _) {
-              vm.onBannerSliderSwiped(index);
-            },
-            viewportFraction: 1,
-            aspectRatio: 375 / 500,
-            // aspectRatio: 337 / 276,
+  List<Widget> _buildTopPositionedContentSliderList() {
+    return List.generate(3, (index) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              '여름엔 청춘',
+              style: AppTextStyle.title2,
+            ),
           ),
-          itemBuilder:
-              (BuildContext context, int itemIndex, int pageViewIndex) {
-            if (vm.isBannerContentsLoaded) {
-              final BannerItem item = vm.bannerContentList![itemIndex];
-              return CachedNetworkImage(
-                imageUrl: item.imgUrl,
-                fit: BoxFit.fitHeight,
-              );
-            } else {
-              return const BannerSkeletonItem();
-            }
-          },
+          AppSpace.size7,
+          Obx(
+            () => ContentPostSlider(
+              height: 158,
+              itemCount: vm.topTenContents?.contentList?.length ?? 5,
+              itemBuilder: (context, index) {
+                if (vm.isTopTenContentsLoaded) {
+                  final item = vm.topTenContents!.contentList![index];
+                  return GestureDetector(
+                    onTap: () {
+                      final argument = ContentArgumentFormat(
+                        contentId: item.contentId,
+                        contentType: item.contentType,
+                        posterImgUrl: item.posterImgUrl,
+                        originId: item.originId,
+                      );
+                      vm.routeToContentDetail(argument, sectionType: 'topTen');
+                    },
+                    child: NewContentPostItem(
+                      imgUrl: vm.topTenContents!.contentList![index]
+                          .posterImgUrl.prefixTmdbImgPath,
+                    ),
+                  );
+                } else {
+                  return const NewContentPostItem(imgUrl: null);
+                }
+              },
+            ),
+          ),
+        ],
+      );
+    });
+  }
+
+  Widget _buildStackedAppBar() => Padding(
+        padding: EdgeInsets.only(top: SizeConfig.to.statusBarHeight) +
+            AppInset.horizontal16,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SvgPicture.asset('assets/icons/new_logo.svg'),
+            // TODO ICON INWELL BUTTON MODULE 수정 필요
+            MaterialButton(
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              minWidth: 0,
+              padding: EdgeInsets.zero,
+              onPressed: vm.routeToSearch,
+              child: SvgPicture.asset(
+                'assets/icons/new_search.svg',
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _buildStackedTopGradient() => IgnorePointer(
+        child: Container(
+          height: 148,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF000000),
+                Color.fromRGBO(15, 15, 15, 0.8177),
+                Color.fromRGBO(15, 15, 15, 0.62),
+                Colors.transparent,
+              ],
+              stops: [0.0, 0.3539, 0.5168, 1.0],
+              // stops: [0.0, 0.1823, 0.375, 1.0],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
         ),
       );
 
@@ -68,47 +130,6 @@ class NewHomeScreen extends BaseScreen<HomeViewModel> {
           );
         },
       );
-
-  /// 상단 'Top10' 포스트 슬라이더
-  List<Widget> _buildTopTenContentSlider() => [
-        AppSpace.size40,
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text(
-            '순삭 Top10',
-            style: AppTextStyle.headline2,
-          ),
-        ),
-        AppSpace.size6,
-        Obx(
-          () => ContentPostSlider(
-            height: 200,
-            itemCount: vm.topTenContents?.contentList?.length ?? 5,
-            itemBuilder: (context, index) {
-              if (vm.isTopTenContentsLoaded) {
-                final item = vm.topTenContents!.contentList![index];
-                return GestureDetector(
-                  onTap: () {
-                    final argument = ContentArgumentFormat(
-                      contentId: item.contentId,
-                      contentType: item.contentType,
-                      posterImgUrl: item.posterImgUrl,
-                      originId: item.originId,
-                    );
-                    vm.routeToContentDetail(argument, sectionType: 'topTen');
-                  },
-                  child: ContentPostItem(
-                    imgUrl: vm.topTenContents!.contentList![index].posterImgUrl
-                        .prefixTmdbImgPath,
-                  ),
-                );
-              } else {
-                return const ContentPostItem(imgUrl: null);
-              }
-            },
-          ),
-        ),
-      ];
 
   /// 배경 위젯 - Poster + Gradient Image 로 구성됨.
   List<Widget> _buildStackedGradientPosterBg() => [
@@ -144,46 +165,180 @@ class NewHomeScreen extends BaseScreen<HomeViewModel> {
           ),
         )
       ];
+}
 
-  /// 애니메이션 앱바 - 스크롤 동작 및 offset에 따라 blur animation이 적용됨.
-  List<Widget> _buildAnimationAppbar() {
-    return [
-      Container(
-        padding: EdgeInsets.only(top: SizeConfig.to.statusBarHeight) +
-            AppInset.horizontal16,
-        height: 148,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF000000),
-              Color.fromRGBO(15, 15, 15, 0.8177),
-              Color.fromRGBO(15, 15, 15, 0.62),
-              Colors.transparent,
-            ],
-            stops: [0.0, 0.3539, 0.5168, 1.0],
-            // stops: [0.0, 0.1823, 0.375, 1.0],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+/// 상단 배너 슬라이더
+class _BannerSlider extends BaseView<HomeViewModel> {
+  const _BannerSlider({Key? key}) : super(key: key);
+
+  @override
+  Widget buildView(BuildContext context) {
+    return Obx(
+      () => Stack(
+        children: [
+          GestureDetector(
+            onTap: (){
+              print(vm.topPositionedCategory.value?.length ?? " NONE ");
+            },
+            child: CarouselSlider.builder(
+              carouselController: vm.carouselController,
+              itemCount: vm.bannerContentList?.length ?? 2,
+              options: CarouselOptions(
+                height: 500,
+                autoPlay: true,
+                autoPlayAnimationDuration: const Duration(milliseconds: 1800),
+                onPageChanged: (index, _) {
+                  vm.onBannerSliderSwiped(index);
+                },
+                onScrolled: vm.onBannerSliderScrolled,
+                viewportFraction: 1,
+                // aspectRatio: 375 / 500,
+                // aspectRatio: 337 / 276,
+              ),
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) {
+                if (vm.isBannerContentsLoaded) {
+                  final BannerItem item = vm.bannerContentList![itemIndex];
+                  return CachedNetworkImage(
+                    imageUrl: item.imgUrl,
+                    fit: BoxFit.fitHeight,
+                  );
+                } else {
+                  return const BannerSkeletonItem();
+                }
+              },
+            ),
           ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SvgPicture.asset('assets/icons/new_logo.svg'),
-            // TODO ICON INWELL BUTTON MODULE 수정 필요
-            MaterialButton(
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              minWidth: 0,
-              padding: EdgeInsets.zero,
-              onPressed: vm.routeToSearch,
-              child: SvgPicture.asset(
-                'assets/icons/new_search.svg',
+
+          // 하단 그레디언트,  Bottom Gradient Container
+          Positioned(
+            bottom: -8,
+            child: IgnorePointer(
+              child: Container(
+                width: SizeConfig.to.screenWidth,
+                height: 156,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(15, 15, 15, 0),
+                      Color.fromRGBO(15, 15, 15, 0.6),
+                      Color.fromRGBO(15, 15, 15, 0.8),
+                      Color.fromRGBO(15, 15, 15, 1),
+                    ],
+                    stops: [0.0, 0.3539, 0.5168, 1.0],
+                    // stops: [0.0, 0.1823, 0.375, 1.0],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+
+          // 콘텐츠 정보 섹션
+          Positioned(
+            bottom: 8,
+            child: SizedBox(
+              width: SizeConfig.to.screenWidth,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '나에게 딱 맞는 콘텐츠',
+                    style: AppTextStyle.title3
+                        .copyWith(color: AppColor.main, letterSpacing: -0.2),
+                  ),
+                  AppSpace.size2,
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 100),
+                    opacity: vm.bannerInfoOpacity.value,
+                    child: Column(
+                      children: [
+                        Text(
+                          vm.selectedBannerContent?.title ?? '',
+                          style:
+                              AppTextStyle.web3.copyWith(letterSpacing: -0.2),
+                        ),
+                        AppSpace.size4,
+                        Text(
+                          '코미디 · 드라마 · 스릴러',
+                          style: AppTextStyle.alert2.copyWith(
+                              color: AppColor.gray03, letterSpacing: -0.2),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AppSpace.size12,
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: DotsIndicator(
+                      dotsCount: vm.bannerContentList?.length ?? 4,
+                      position: vm.bannerContentsSliderIndex.value.toDouble(),
+                      decorator: const DotsDecorator(
+                        spacing: EdgeInsets.symmetric(horizontal: 4),
+                        size: Size(4, 4),
+                        activeSize: Size(4, 4),
+                        color: AppColor.gray05,
+                        // Inactive color
+                        activeColor: AppColor.gray02,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-    ];
+    );
+  }
+}
+
+class _TopTenContentSlider extends BaseView<HomeViewModel> {
+  const _TopTenContentSlider({Key? key}) : super(key: key);
+
+  @override
+  Widget buildView(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AppSpace.size40,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            '지금 뜨고있는 인기 콘텐츠',
+            style: AppTextStyle.title2,
+          ),
+        ),
+        AppSpace.size7,
+        Obx(
+          () => ContentPostSlider(
+            height: 158,
+            itemCount: vm.topTenContents?.contentList?.length ?? 5,
+            itemBuilder: (context, index) {
+              if (vm.isTopTenContentsLoaded) {
+                final item = vm.topTenContents!.contentList![index];
+                return GestureDetector(
+                  onTap: () {
+                    final argument = ContentArgumentFormat(
+                      contentId: item.contentId,
+                      contentType: item.contentType,
+                      posterImgUrl: item.posterImgUrl,
+                      originId: item.originId,
+                    );
+                    vm.routeToContentDetail(argument, sectionType: 'topTen');
+                  },
+                  child: ContentPostItem(
+                    imgUrl: vm.topTenContents!.contentList![index].posterImgUrl
+                        .prefixTmdbImgPath,
+                  ),
+                );
+              } else {
+                return const ContentPostItem(imgUrl: null);
+              }
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
