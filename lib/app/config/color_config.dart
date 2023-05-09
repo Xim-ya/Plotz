beabfb0346aaf07809a6404e2bd2bfca94aca4d5
XimYa <hj_lee_com@naver.com>
2023-05-09 10:40:33 +0900

    feature : 새로운 color 리소스 추가

--- lib/app/config/color_config.dart
+++ lib/app/config/color_config.dart
@@ -3,13 +3,23 @@ import 'package:soon_sak/utilities/index.dart';
 abstract class AppColor {
   AppColor._();
 
-
+  static const main = Color(0xFF0DE17B);
   static const black = Color(0xFF0F0F0F);
   static const darkGrey = Color(0xFF212121); // Used in Bottom Navigation
   static const lightGrey = Color(0xFFBBBAC1); // Used in Home Sub title;
   static const mixedWhite = Color(0xFFE6E6E6);
+  static const white = Color(0xFFFFFFFF);
   static const strongGrey = Color(0xFF1F1F20);
   static const yellow = Color(0xFFFFE24B);
   static const red = Color(0xFFFF484E);
   static const green = Color(0xFF34C759);
+
+  // New None
+  static const gray01 = Color(0xFFE7E7E7);
+  static const gray02 = Color(0xFFBEBEBE);
+  static const gray03 = Color(0xFF9D9D9D);
+  static const gray04 = Color(0xFF606060);
+  static const gray05 = Color(0xFF3E3E3E);
+  static const gray06 = Color(0xFF303030);
+  static const gray07 = Color(0xFF1F1F1F);
 }
