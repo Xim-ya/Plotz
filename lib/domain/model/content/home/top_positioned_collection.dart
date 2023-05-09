2054f66a566b5d31fb3eee85879b6942047ac54b
XimYa <hj_lee_com@naver.com>
2023-05-09 10:49:16 +0900

    feature : 상단 노출 콘텐츠 콜렉션 모델 추가

---
+++ lib/domain/model/content/home/top_positioned_collection.dart
@@ -0,0 +1,33 @@
+import 'package:soon_sak/data/api/staticContent/response/new_category_content_item_response.dart';
+import 'package:soon_sak/data/api/staticContent/response/top_positioned_collection_response.dart';
+import 'package:soon_sak/domain/model/content/home/new_content_poster_shell.dart';
+
+class TopPositionedCollection {
+  final String key;
+  final List<TopPositionedCategory> categories;
+
+  TopPositionedCollection({required this.key, required this.categories});
+
+  factory TopPositionedCollection.fromResponse(
+          TopPositionedCollectionResponse response) =>
+      TopPositionedCollection(
+          key: response.key,
+          categories: response.categories
+              .map((e) => TopPositionedCategory.fromResponse(e))
+              .toList());
+}
+
+class TopPositionedCategory {
+  final String title;
+  final List<NewContentPosterShell> items;
+
+  TopPositionedCategory({required this.title, required this.items});
+
+  factory TopPositionedCategory.fromResponse(
+          TopPositionedCategoryResponse response) =>
+      TopPositionedCategory(
+          title: response.title,
+          items: response.items
+              .map((e) => NewContentPosterShell.fromResponse(e))
+              .toList());
+}
