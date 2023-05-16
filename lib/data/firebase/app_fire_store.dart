12b7ace087e95dffd70ddf71125ed65336350801
XimYa <hj_lee_com@naver.com>
2023-05-16 23:20:51 +0900

    chore : 임시 코드

--- lib/data/firebase/app_fire_store.dart
+++ lib/data/firebase/app_fire_store.dart
@@ -7,7 +7,8 @@ import 'package:soon_sak/utilities/index.dart';
 abstract class AppFireStore {
   AppFireStore._(); // 생성자를 private으로 설정
 
-  static final FirebaseFirestore _db = FirebaseFirestore.instance; // 인스턴스를 private로 선언하고 초기화
+  static final FirebaseFirestore _db =
+      FirebaseFirestore.instance; // 인스턴스를 private로 선언하고 초기화
 
   static FirebaseFirestore get getInstance => _db; // public getter를 통해 인스턴스에 접근
 }
