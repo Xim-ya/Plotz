0b3b6c551d534b0eee6c93f9116abc5d67e37119
XimYa <hj_lee_com@naver.com>
2023-06-09 13:49:29 +0900

    feature : safe di 로직 추가

--- lib/app/di/locator/locator.dart
+++ lib/app/di/locator/locator.dart
@@ -1,3 +1,9 @@
 import 'package:get_it/get_it.dart';
 
 final GetIt locator = GetIt.I;
+
+void unregisterIfRegistered<T extends Object>() {
+  if (locator.isRegistered<T>()) {
+    locator.unregister<T>();
+  }
+}
