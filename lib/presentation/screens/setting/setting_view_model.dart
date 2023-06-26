38bf85115bf991d03bc3fc325530e1891758153f
XimYa <hj_lee_com@naver.com>
2023-06-27 01:37:10 +0900

    feature : 로그아웃 및 회원탈퇴 시 내부 유저 로컬 데이터를 삭제하는 로직 추가

--- lib/presentation/screens/setting/setting_view_model.dart
+++ lib/presentation/screens/setting/setting_view_model.dart
@@ -27,6 +27,7 @@ class SettingViewModel extends BaseViewModel {
     response.fold(
       onSuccess: (data) {
         signOut().whenComplete(() {
+          clearUserLocalData();
           AlertWidget.newToast(message: '회원탈퇴 처리 되었습니다', context);
         });
       },
@@ -77,12 +78,24 @@ class SettingViewModel extends BaseViewModel {
     result.fold(
       onSuccess: (_) {
         context.go(AppRoutes.login);
+        clearUserLocalData();
         TabsBinding.unRegisterDependencies();
         LoginBinding.dependencies();
       },
       onFailure: (e) {
         AlertWidget.newToast(message: '로그아웃에 실패했습니다. 다시 시도 시도해주세요', context);
         log(e.toString());
+      },
+    );
+  }
+
+  // 유저 로컬 데이터 삭제
+  void clearUserLocalData() {
+    final response = _userRepository.clearUserLocalData();
+    response.fold(
+      onSuccess: (_) {},
+      onFailure: (e) {
+        log('SettingViewModel : $e');
       },
     );
   }
